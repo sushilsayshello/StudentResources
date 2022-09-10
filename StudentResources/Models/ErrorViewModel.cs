022f9f4b8271a2b671b57be0cdd0e60a6b9de846
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-10 13:37:43 +1000

    Update ErrorViewModel.cs

--- StudentResources/Models/ErrorViewModel.cs
+++ StudentResources/Models/ErrorViewModel.cs
@@ -2,10 +2,5 @@ using System;
 
 namespace StudentResources.Models
 {
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
 }
