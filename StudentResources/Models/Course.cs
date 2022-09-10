97fd27574760366f08ad7fda7f42a22c5b53ec0f
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-10 13:37:23 +1000

    Update Course.cs

--- StudentResources/Models/Course.cs
+++ StudentResources/Models/Course.cs
@@ -5,5 +5,16 @@ using System.Threading.Tasks;
 
 namespace StudentResources.Models
 {
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseUnits { get; set; }
+        public int UnitId { get; set; }
+        public string UnitSpecification { get; set; }
+        public int UnitTeacherId { get; set; }
+        public string TeacherName { get; set; }
+        public bool Approval { get; set; }
+    }
 
 }
