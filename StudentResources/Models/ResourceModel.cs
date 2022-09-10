2b55ef7050545ffc3a6e09717ecb3644f4dbb47a
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-10 13:39:03 +1000

    Update ResourceModel.cs

--- StudentResources/Models/ResourceModel.cs
+++ StudentResources/Models/ResourceModel.cs
@@ -5,5 +5,13 @@ using System.Threading.Tasks;
 
 namespace StudentResources.Models
 {
+    public class ResourceModel
+    {
+        public int ResourceId { get; set; }
+        public string ResourceName { get; set; }
+        public string ResourceType { get; set; }
+        public bool ResourceAvailability { get; set; }
+        public int ResourceQuantity { get; set; }
+    }
 
 }
