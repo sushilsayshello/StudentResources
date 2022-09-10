18245b5e9a8a743d33f38ea8bd7ed347fa96be1d
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-10 13:39:24 +1000

    Update StudentModel.cs

--- StudentResources/Models/StudentModel.cs
+++ StudentResources/Models/StudentModel.cs
@@ -5,5 +5,14 @@ using System.Threading.Tasks;
 
 namespace StudentResources.Models
 {
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
 
 }
