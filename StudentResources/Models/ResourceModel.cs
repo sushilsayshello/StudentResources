291361f2cc555fd0a276a7a98469c720000a0f17
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-12 23:35:48 +1000

    Update ResourceModel.cs

--- StudentResources/Models/ResourceModel.cs
+++ StudentResources/Models/ResourceModel.cs
@@ -7,11 +7,7 @@ namespace StudentResources.Models
 {
     public class ResourceModel
     {
-        public int ResourceId { get; set; }
-        public string ResourceName { get; set; }
-        public string ResourceType { get; set; }
-        public bool ResourceAvailability { get; set; }
-        public int ResourceQuantity { get; set; }
+
     }
 
 }
