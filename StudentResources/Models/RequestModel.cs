3938017adf945bd9379f13815a1cda2d47af7464
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-09-12 23:35:24 +1000

    Update RequestModel.cs

--- StudentResources/Models/RequestModel.cs
+++ StudentResources/Models/RequestModel.cs
@@ -1,1 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace StudentResources.Models
+{
+    public class RequestModel
+    {
+
+
+    }
+}
